89702a76ef9160687999c7e7d6564dd90b92594b
Mayara Abreu <mayara.arj@gmail.com>
2022-07-31 17:46:02 -0300

    Escrevendo e lendo dados no banco

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers
@@ -8,54 +9,51 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        //Declarando e iniciando FilmeContext no controlador.
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
+
+        //O IActionResult se refere às respostas do método HTTP.
 
         [HttpPost]
         public IActionResult AdicionarFilme([FromBody] Filme filme)
         {
-
-            filme.Id = id++;
-            filmes.Add(filme);
+            //Adicionando filme ao banco de dados
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
 
             // CreatedAtAction retorna a ação que criou este recurso. É uma convenção de boas prátcas usar juntamente com o verbo HttpPost para informar onde o recurso pode ser acessado.
             // nameof para indicar que vou passar o nome da action que precisou executar para recuperar o recurso, que no caso é RecuperaFilmeporID
             // no segundo parâmetro, colocamos os valores que usamos na rota, que no caso é o id do filme que acabamos de criar.
             // no terceiro parâmetro passamos o object em si que estamos tratando, que no caso é o filme.
-            return CreatedAtAction(nameof(RecuperaFilmeporID), new { Id = filme.Id}, filme);
+            return CreatedAtAction(nameof(RecuperaFilmeporID), new { Id = filme.Id }, filme);
 
         }
 
 
-        //O IActionResult se refere às respostas do método HTTP.
+
         [HttpGet]
-        public IActionResult RecuperarFilmes()
+        public IEnumerable<Filme> RecuperarFilmes()
         {
-            return Ok(filmes);
+            return _context.Filmes;
         }
 
         //Na busca, usa-se o parâmetro na url. Ex: http://localhost:5000/filmes/4. Se não passar, cai na rota do get geral.
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmeporID(int id)
         {
-          Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if (filme != null)
             {
                 return Ok(filme);
             }
             return NotFound();
         }
-      
-        // Essa sintaxe foi substituída nas linhas acima por uma sintaxe própria do C#
-        //{
-        //    //foreach( Filme filme in filmes)
-        //    //{
-        //    //    if (filme.Id == id)
-        //    //    {
-        //    //        return filme;
-        //    //    }
-        //    //}
-        //    //return null;
-        //}
+
+
     }
 }
